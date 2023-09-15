3ab1b6f1036432890f5d3dcf6b6d9c3cf0fa496f
AbhishekBhanot5991 <abhishekbhanot5991@gmail.com>
2023-09-15 17:27:23 +0530

    initial commit

--- seventh-playground/main.js
+++ seventh-playground/main.js
@@ -4,6 +4,6 @@ const cursor = document.querySelector('.cursor');
 // cursor.style.backgroundColor= "green"
 main.addEventListener('mousemove', (dot)=>{
     console.log(dot)
-    cursor.style.left=dot.x+"px"
-    cursor.style.top=dot.y+"px"
+    cursor.style.left = dot.x+"px"
+    cursor.style.top = dot.y+"px"
 })
